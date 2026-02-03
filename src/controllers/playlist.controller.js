646f661350ab8dca35d294033e3e48ccedc66796
aaditya0204 <adityarajcareer123@gmail.com>
2026-02-04 01:33:51 +0530

    Updated playlist controller to include authentication middleware for secure access.

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,6 +3,7 @@ import { PLAYLIST, Playlist } from "../models/playlist.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { VIDEO } from "../models/video.model.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
   const { name, description } = req.body;
@@ -21,15 +22,48 @@ const createPlaylist = asyncHandler(async (req, res) => {
 const getUserPlaylists = asyncHandler(async (req, res) => {
   const { userId } = req.params;
   //TODO: get user playlists
+  const playlists = await PLAYLIST.find({ owner: userId });
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, playlists, "User playlists fetched successfully")
+    );
 });
 
 const getPlaylistById = asyncHandler(async (req, res) => {
   const { playlistId } = req.params;
   //TODO: get playlist by id
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlist ID");
+  }
+  const playlist = await PLAYLIST.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(404, "Playlist not found");
+  }
+  res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "Playlist fetched successfully"));
 });
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
   const { playlistId, videoId } = req.params;
+  if (!isValidObjectId(playlistId) && isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid playlist ID OR Video Id");
+  }
+  const video = await VIDEO.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+  const playlist = await PLAYLIST.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(404, "Playlist not found");
+  }
+  await PLAYLIST.findByIdAndUpdate(playlistId, {
+    $addToSet: { videos: videoId },
+  });
+  res
+    .status(200)
+    .json(new ApiResponse(200, null, "Video added to playlist successfully"));
 });
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
